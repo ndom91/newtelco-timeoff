dd4f08287b76f0cc22a1bfee38e8b8598ad9ed6d
ndo@ndo1 <yo@ni.co.de>
2019-11-21 17:08:49 +0100

    refactor: sidebar into own component

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -1,65 +1,16 @@
 import React from 'react'
 import Router from 'next/router'
-import Link from 'next/link'
 import { NextAuth } from 'next-auth/client'
 import fetch from 'isomorphic-unfetch'
+import SidebarNT from '../../components/sidebarnt'
 import '../../style/newtelco-rsuite.less'
 import {
-  Nav,
-  Navbar,
-  Dropdown,
-  Sidenav,
-  Sidebar,
-  Icon,
   Container,
   Header,
   Content,
   Footer,
   Breadcrumb
 } from 'rsuite'
-import NTLogo from '../../../public/static/img/newtelco_letters.svg'
-
-const NavToggle = ({ expand, onChange }, props) => {
-  return (
-    <Navbar appearance='subtle' className='nav-toggle'>
-      <Navbar.Body>
-        <Nav>
-          <Dropdown
-            placement='topStart'
-            trigger='click'
-            renderTitle={children => {
-              return <Icon className='icon-style' icon='cog' />
-            }}
-          >
-            <Dropdown.Item>Help</Dropdown.Item>
-            <Dropdown.Item>Settings</Dropdown.Item>
-            <Dropdown.Item>
-              <form id='signout' method='post' action='/auth/signout' onSubmit={props.handleSignOut}>
-                <input name='_csrf' type='hidden' value={props.token} />
-                <div className='logout-btn-wrapper'>
-                  <button
-                    className='logout-btn'
-                    type='submit'
-                    onClick={(ev) => ev.preventDefault()}
-                  >
-                    Sign out
-                  </button>
-                </div>
-              </form>
-
-            </Dropdown.Item>
-          </Dropdown>
-        </Nav>
-
-        <Nav pullRight>
-          <Nav.Item onClick={onChange} style={{ width: 56, textAlign: 'center' }}>
-            <Icon icon={expand ? 'angle-left' : 'angle-right'} />
-          </Nav.Item>
-        </Nav>
-      </Navbar.Body>
-    </Navbar>
-  )
-}
 
 class Layout extends React.Component {
   constructor (props) {
@@ -128,101 +79,10 @@ class Layout extends React.Component {
   }
 
   render () {
-    const { expand } = this.state
-
     return (
       <div className='show-fake-browser sidebar-page wrapper'>
         <Container>
-          <Sidebar
-            style={{ display: 'flex', flexDirection: 'column' }}
-            width={expand ? 260 : 56}
-            collapsible
-          >
-            <Sidenav.Header>
-              <div className='sidenav-header'>
-                <img src={NTLogo} alt='Logo' style={{ height: '32px', width: '32px', marginTop: '-5px', marginLeft: '-5px' }} />
-                <h3 style={{ display: 'inline', fontWeight: '200', marginLeft: 12 }}>
-                  {this.state.settings.companyName}
-                </h3>
-              </div>
-            </Sidenav.Header>
-            <Sidenav
-              expanded={expand}
-              defaultOpenKeys={['4', '5']}
-              appearance='default'
-            >
-              <Sidenav.Body>
-                <Nav>
-                  <Link href='/'>
-                    <Nav.Item eventKey='1' active={typeof window !== 'undefined' && Router.pathname === '/'} icon={<Icon icon='area-chart' />}>
-                      Dashboard
-                    </Nav.Item>
-                  </Link>
-                  <Link href='/user'>
-                    <Nav.Item eventKey='2' active={typeof window !== 'undefined' && Router.pathname === '/user'} icon={<Icon icon='user' />}>
-                      User
-                    </Nav.Item>
-                  </Link>
-                  <Link href='/new'>
-                    <Nav.Item eventKey='3' active={typeof window !== 'undefined' && Router.pathname === '/new'} icon={<Icon icon='plus-square' />}>
-                      New
-                    </Nav.Item>
-                  </Link>
-                  <Dropdown
-                    eventKey='4'
-                    trigger='hover'
-                    title='Team'
-                    icon={<Icon icon='group' />}
-                    placement='rightStart'
-                    active={typeof window !== 'undefined' && Router.pathname.includes('team')}
-                  >
-                    <Link href='/team/dashboard'>
-                      <Dropdown.Item
-                        eventKey='4-1'
-                        active={typeof window !== 'undefined' && Router.pathname === '/team/dashboard'}
-                      >
-                        Dashboard
-                      </Dropdown.Item>
-                    </Link>
-                    <Link href='/team/calendar'>
-                      <Dropdown.Item
-                        eventKey='4-2'
-                        active={typeof window !== 'undefined' && Router.pathname === '/team/calendar'}
-                      >
-                        Calendar
-                      </Dropdown.Item>
-                    </Link>
-                  </Dropdown>
-                  <Dropdown
-                    eventKey='5'
-                    trigger='hover'
-                    title='Settings'
-                    icon={<Icon icon='cog' />}
-                    placement='rightStart'
-                    active={typeof window !== 'undefined' && Router.pathname.includes('settings')}
-                  >
-                    <Link href='/settings/general'>
-                      <Dropdown.Item
-                        eventKey='5-1'
-                        active={typeof window !== 'undefined' && Router.pathname === '/settings/general'}
-                      >
-                        General
-                      </Dropdown.Item>
-                    </Link>
-                    <Link href='/settings/admin'>
-                      <Dropdown.Item
-                        eventKey='5-2'
-                        active={typeof window !== 'undefined' && Router.pathname === '/settings/admin'}
-                      >
-                        Admin
-                      </Dropdown.Item>
-                    </Link>
-                  </Dropdown>
-                </Nav>
-              </Sidenav.Body>
-            </Sidenav>
-            <NavToggle expand={expand} handleSignOut={this.onSignOutSubmit} token={this.props.token} onChange={this.handleToggle} />
-          </Sidebar>
+          <SidebarNT />
           <Container className='wrapper'>
             <Header>
               <div className='header-wrapper'>
---
+++ src/components/sidebarnt.js
@@ -0,0 +1,201 @@
+import React from 'react'
+import Link from 'next/link'
+import Router from 'next/router'
+import fetch from 'isomorphic-unfetch'
+import {
+  Nav,
+  Navbar,
+  Dropdown,
+  Sidenav,
+  Sidebar,
+  Icon
+} from 'rsuite'
+import NTLogo from '../../public/static/img/newtelco_letters.svg'
+
+const NavToggle = ({ expand, onChange }, props) => {
+  return (
+    <Navbar appearance='subtle' className='nav-toggle'>
+      <Navbar.Body>
+        <Nav>
+          <Dropdown
+            placement='topStart'
+            trigger='click'
+            renderTitle={children => {
+              return <Icon className='icon-style' icon='cog' />
+            }}
+          >
+            <Dropdown.Item>Help</Dropdown.Item>
+            <Dropdown.Item>Settings</Dropdown.Item>
+            <Dropdown.Item>
+              <form id='signout' method='post' action='/auth/signout' onSubmit={props.handleSignOut}>
+                <input name='_csrf' type='hidden' value={props.token} />
+                <div className='logout-btn-wrapper'>
+                  <button
+                    className='logout-btn'
+                    type='submit'
+                    onClick={(ev) => ev.preventDefault()}
+                  >
+                    Sign out
+                  </button>
+                </div>
+              </form>
+
+            </Dropdown.Item>
+          </Dropdown>
+        </Nav>
+
+        <Nav pullRight>
+          <Nav.Item onClick={onChange} style={{ width: 56, textAlign: 'center' }}>
+            <Icon icon={expand ? 'angle-left' : 'angle-right'} />
+          </Nav.Item>
+        </Nav>
+      </Navbar.Body>
+    </Navbar>
+  )
+}
+class SidebarNT extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      expand: false,
+      settings: {
+        companyName: ''
+      }
+    }
+  }
+
+  componentDidMount () {
+    const protocol = window.location.protocol
+    const host = window.location.host
+    const companyInfo = JSON.parse(window.localStorage.getItem('company'))
+    const expandStorage = window.localStorage.getItem('layout-expand')
+    if (companyInfo) {
+      this.setState({
+        expand: expandStorage === 'true',
+        settings: {
+          companyName: companyInfo.companyName
+        }
+      })
+    } else {
+      fetch(`${protocol}//${host}/api/settings/company/info`)
+        .then(res => res.json())
+        .then(data => {
+          if (data) {
+            window.localStorage.setItem('company', JSON.stringify(data.companyInfo[0]))
+            this.setState({
+              settings: {
+                companyName: data.companyInfo[0].companyName
+              }
+            })
+          }
+        })
+        .catch(err => console.error(err))
+    }
+  }
+
+  render () {
+    const { expand } = this.state
+
+    return (
+      <Sidebar
+        style={{ display: 'flex', flexDirection: 'column' }}
+        // width={expand ? 260 : 56}
+        collapsible
+        className='sidebar-wrapper'
+      >
+        <Sidenav.Header>
+          <div className='sidenav-header'>
+            <img src={NTLogo} alt='Logo' style={{ height: '32px', width: '32px', marginTop: '-5px', marginLeft: '-5px' }} />
+            <h3 style={{ display: 'inline', fontWeight: '200', marginLeft: 12 }}>
+              {this.state.settings.companyName}
+            </h3>
+          </div>
+        </Sidenav.Header>
+        <Sidenav
+          expanded={expand}
+          defaultOpenKeys={['4', '5']}
+          appearance='default'
+        >
+          <Sidenav.Body>
+            <Nav>
+              <Link href='/'>
+                <Nav.Item eventKey='1' active={typeof window !== 'undefined' && Router.pathname === '/'} icon={<Icon icon='area-chart' />}>
+                      Dashboard
+                </Nav.Item>
+              </Link>
+              <Link href='/user'>
+                <Nav.Item eventKey='2' active={typeof window !== 'undefined' && Router.pathname === '/user'} icon={<Icon icon='user' />}>
+                      User
+                </Nav.Item>
+              </Link>
+              <Link href='/new'>
+                <Nav.Item eventKey='3' active={typeof window !== 'undefined' && Router.pathname === '/new'} icon={<Icon icon='plus-square' />}>
+                      New
+                </Nav.Item>
+              </Link>
+              <Dropdown
+                eventKey='4'
+                trigger='hover'
+                title='Team'
+                icon={<Icon icon='group' />}
+                placement='rightStart'
+                active={typeof window !== 'undefined' && Router.pathname.includes('team')}
+              >
+                <Link href='/team/dashboard'>
+                  <Dropdown.Item
+                    eventKey='4-1'
+                    active={typeof window !== 'undefined' && Router.pathname === '/team/dashboard'}
+                  >
+                        Dashboard
+                  </Dropdown.Item>
+                </Link>
+                <Link href='/team/calendar'>
+                  <Dropdown.Item
+                    eventKey='4-2'
+                    active={typeof window !== 'undefined' && Router.pathname === '/team/calendar'}
+                  >
+                        Calendar
+                  </Dropdown.Item>
+                </Link>
+              </Dropdown>
+              <Dropdown
+                eventKey='5'
+                trigger='hover'
+                title='Settings'
+                icon={<Icon icon='cog' />}
+                placement='rightStart'
+                active={typeof window !== 'undefined' && Router.pathname.includes('settings')}
+              >
+                <Link href='/settings/general'>
+                  <Dropdown.Item
+                    eventKey='5-1'
+                    active={typeof window !== 'undefined' && Router.pathname === '/settings/general'}
+                  >
+                        General
+                  </Dropdown.Item>
+                </Link>
+                <Link href='/settings/admin'>
+                  <Dropdown.Item
+                    eventKey='5-2'
+                    active={typeof window !== 'undefined' && Router.pathname === '/settings/admin'}
+                  >
+                        Admin
+                  </Dropdown.Item>
+                </Link>
+              </Dropdown>
+            </Nav>
+          </Sidenav.Body>
+        </Sidenav>
+        <NavToggle expand={expand} handleSignOut={this.onSignOutSubmit} token={this.props.token} onChange={this.handleToggle} />
+        <style jsx>{`
+          :global(.sidebar-wrapper) {
+            width: ${this.state.expand ? '260px' : '56px'};
+          }
+        `}
+        </style>
+      </Sidebar>
+    )
+  }
+}
+
+export default SidebarNT
