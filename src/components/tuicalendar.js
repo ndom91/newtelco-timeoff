271632db1064f8d96dcf78306f4ebff03552d085
ndo@ndo1 <yo@ni.co.de>
2020-01-06 16:34:03 +0100

    update: team calendar

--- src/components/tuicalendar.js
+++ src/components/tuicalendar.js
@@ -1,16 +1,21 @@
 import React from 'react'
 import Calendar from '@toast-ui/react-calendar'
 import 'tui-calendar/dist/tui-calendar.css'
+import 'tui-date-picker/dist/tui-date-picker.css'
+import 'tui-time-picker/dist/tui-time-picker.css'
 import fetch from 'isomorphic-unfetch'
+import { Container, Header, Button, IconButton, ButtonGroup, Icon, ButtonToolbar } from 'rsuite'
 
-// https://nicedoc.io/nhn/toast-ui.react-calendar#user-content--install
+// https://github.com/nhn/toast-ui.react-calendar
 
 class TuiCalendar extends React.Component {
   constructor (props) {
     super(props)
+    this.calendarRef = React.createRef()
 
     this.state = {
-      teamSchedules: []
+      teamSchedules: [],
+      currentView: 'month'
     }
   }
 
@@ -31,33 +36,132 @@ class TuiCalendar extends React.Component {
       .catch(err => console.error(err))
   }
 
+  handleToggleView = () => {
+    const {
+      currentView
+    } = this.state
+
+    console.log(this.calendarRef)
+    const calendarInstance = this.calendarRef.current.getInstance()
+    if (currentView === 'month') {
+      calendarInstance.changeView('week', true)
+      this.setState({
+        currentView: 'week'
+      })
+    } else {
+      calendarInstance.changeView('month', true)
+      this.setState({
+        currentView: 'month'
+      })
+    }
+  }
+
   render () {
+    const {
+      currentView
+    } = this.state
+
     return (
-      <Calendar
-        height='auto'
-        calendars={[
-          {
-            id: '0',
-            name: 'Private',
-            bgColor: '#9e5fff',
-            borderColor: '#9e5fff'
-          }
-        ]}
-        disableDblClick
-        disableClick={false}
-        isReadOnly
-        month={{
-          startDayOfWeek: 0
-        }}
-        defaultView='month'
-        timezones={[
-          {
-            timezoneOffset: 60,
-            displayLabel: 'GMT+01:00',
-            tooltip: 'Berlin'
-          }
-        ]}
-      />
+      <Container>
+        <Header className='calendar-header'>
+          <span
+            style={{
+              fontSize: '24px'
+            }}
+          >
+            {this.props.teamName} Calendar
+          </span>
+          <ButtonGroup>
+            <IconButton
+              icon={<Icon icon='calendar' />}
+              onClick={this.handleToggleView}
+              appearance='primary'
+            >
+              Toggle View
+            </IconButton>
+          </ButtonGroup>
+        </Header>
+        <Calendar
+          ref={this.calendarRef}
+          calendars={[
+            {
+              id: '0',
+              name: 'Private',
+              bgColor: '#9e5fff',
+              borderColor: '#9e5fff'
+            }
+          ]}
+          disableDblClick
+          disableClick={false}
+          isReadOnly
+          height='100%'
+          month={{
+            startDayOfWeek: 0,
+            narrowWeekend: true,
+            visibleWeeksCount: 3
+          }}
+          taskView={false}
+          defaultView={currentView}
+          view={currentView}
+          timezones={[
+            {
+              timezoneOffset: 60,
+              displayLabel: 'GMT+01:00',
+              tooltip: 'Berlin'
+            }
+          ]}
+          schedules={[
+            {
+              id: '1',
+              calendarId: '0',
+              title: 'TOAST UI Calendar Study',
+              category: 'time',
+              dueDateClass: '',
+              start: '2020-01-05T14:48:00.000Z',
+              end: '2020-01-06T14:48:00.000Z'
+            },
+            {
+              id: '2',
+              calendarId: '0',
+              title: 'Practice',
+              category: 'milestone',
+              dueDateClass: '',
+              start: '2020-01-05T09:48:00.000Z',
+              end: '2020-01-06T10:48:00.000Z',
+              isReadOnly: true
+            },
+            {
+              id: '3',
+              calendarId: '0',
+              title: 'FE Workshop',
+              category: 'allday',
+              dueDateClass: '',
+              start: '2020-01-07T09:48:00.000Z',
+              end: '2020-01-08T10:48:00.000Z',
+              isReadOnly: true
+            },
+            {
+              id: '4',
+              calendarId: '0',
+              title: 'Report',
+              category: 'time',
+              dueDateClass: '',
+              start: '2020-01-08T09:48:00.000Z',
+              end: '2020-01-09T10:48:00.000Z'
+            }
+          ]}
+        />
+        <style jsx global>{`
+            .calendar-header {
+              width: 100%;
+              display: flex;
+              justify-content: space-between;
+              align-items: center;
+              margin-bottom: 10px;
+            }
+          `}
+        </style>
+      </Container>
     )
   }
 }
--- src/pages/team/calendar.js
+++ src/pages/team/calendar.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Layout from '../../components/layout/index'
 import Router from 'next/router'
+import fetch from 'isomorphic-unfetch'
 import dynamic from 'next/dynamic'
 import { NextAuth } from 'next-auth/client'
 import RequireLogin from '../../components/requiredLogin'
@@ -31,16 +32,53 @@ class Wrapper extends React.Component {
     }
   }
 
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      teamName: ''
+    }
+  }
+
+  componentDidMount () {
+    const protocol = window.location.protocol
+    const host = window.location.host
+    const team = JSON.parse(window.localStorage.getItem('userTeam'))
+    if (team.team) {
+      fetch(`${protocol}//${host}/api/settings/team/members?team=${team.team}`)
+        .then(res => res.json())
+        .then(data => {
+          if (data.teamMembers) {
+            const teamMembers = data.teamMembers.filter(member => {
+              if (member.lname !== 'Cleese' &&
+                member.lname !== 'Device') {
+                return member
+              }
+            })
+            this.setState({
+              team: teamMembers
+            })
+          }
+        })
+        .catch(err => console.error(err))
+
+      this.setState({
+        teamName: team.team
+      })
+    }
+  }
+
   render () {
+    const {
+      teamName
+    } = this.state
+
     if (this.props.session.user) {
       return (
         <Layout user={this.props.session.user.email} token={this.props.session.csrfToken}>
           <Container>
-            <Header>
-              Lorem Ipsum
-            </Header>
             <Content>
-              {typeof window !== 'undefined' && <TuiCalendar />}
+              {typeof window !== 'undefined' && <TuiCalendar teamName={teamName} />}
             </Content>
           </Container>
         </Layout>
