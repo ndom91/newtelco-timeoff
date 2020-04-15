8689219e32e240070f58fe5ee9402eff3a8147fd
ndo@ndo3 <yo@ni.co.de>
2020-04-16 00:38:03 +0200

    update: new request auto-fill

---
+++ src/pages/api/user/entries/new.js
@@ -0,0 +1,10 @@
+const db = require('../../../../lib/db')
+const escape = require('sql-template-strings')
+
+module.exports = async (req, res) => {
+  const email = req.query.email
+  const newestEntry = await db.query(escape`
+    SELECT id, name, email, resturlaubVorjahr, jahresurlaubInsgesamt, jahresUrlaubAusgegeben, restjahresurlaubInsgesamt, beantragt, resturlaubJAHR, type, DATE_FORMAT(fromDate, \"%d.%m.%Y\") as fromDate, DATE_FORMAT(toDate, \"%d.%m.%Y\") as toDate, manager, note, files, submitted_datetime, submitted_by, approval_hash, approved, approval_datetime, disabled FROM vacations WHERE disabled LIKE 0 AND type LIKE 'vacation' AND email like ${email} AND disabled like 0 AND approved LIKE 2 order by submitted_datetime desc limit 1
+  `)
+  res.status(200).json({ newestEntry })
+}
