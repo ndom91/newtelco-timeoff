f75c564c5664fe6473bfd9c91d9963c41d5923d7
ndo@ndo3 <yo@ndo.dev>
2020-04-17 21:02:28 +0200

    add: user/entries/new api call

---
+++ src/pages/api/user/entries/new.js
@@ -0,0 +1,10 @@
+const db = require('../../../../lib/db')
+const escape = require('sql-template-strings')
+
+module.exports = async (req, res) => {
+  const type = req.query.t
+  const userEntries = await db.query(escape`
+    SELECT id, name, email, resturlaubVorjahr, jahresurlaubInsgesamt, jahresUrlaubAusgegeben, restjahresurlaubInsgesamt, beantragt, resturlaubJAHR, type, DATE_FORMAT(fromDate, \"%d.%m.%Y\") as fromDate, DATE_FORMAT(toDate, \"%d.%m.%Y\") as toDate, manager, note, files, submitted_datetime, submitted_by, approval_hash, approved, approval_datetime, disabled FROM vacations WHERE disabled LIKE 0 AND type LIKE ${type}
+  `)
+  res.status(200).json({ userEntries })
+}
